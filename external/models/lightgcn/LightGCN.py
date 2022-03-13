9fd929789cb7da9a4e8c0b40c22933e4675b1625
Daniele Malitesta <57352389+danielemalitesta@users.noreply.github.com>
2022-03-13 15:45:20 +0100

    Update LightGCN.py

--- external/models/lightgcn/LightGCN.py
+++ external/models/lightgcn/LightGCN.py
@@ -1,12 +1,3 @@
-"""
-Module description:
-
-"""
-
-__version__ = '0.3.0'
-__author__ = 'Vito Walter Anelli, Claudio Pomo, Daniele Malitesta, Felice Antonio Merra'
-__email__ = 'vitowalter.anelli@poliba.it, claudio.pomo@poliba.it, daniele.malitesta@poliba.it, felice.merra@poliba.it'
-
 from tqdm import tqdm
 import numpy as np
 import torch
